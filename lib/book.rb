c65074ce87c10b064f0bb247277ccd0938aaa2e8
Jared  E <jared.epps322@gmail.com>
2020-05-13 02:00:57 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,6 @@
-
-
 class Book
+  
   attr_accessor :author, :page_count, :genre 
-  attr_reader :tittle 
   
   def initialize(title)
     @title = title 
@@ -11,34 +9,74 @@ class Book
   def title 
     @title
   end 
+
+  def turn_page 
+    puts "Flipping the page...wow, you read fast!"
+  end 
+    
   
-  def author=(author)
-    @author = author 
 end 
 
-def author 
-  @author 
-end 
 
-def page_count(num)
-  @page_count = num 
-end 
 
-def page_count
-  @page_count 
-end 
 
-def genre=(genre)
-  @genre = genre 
-end 
 
-def genre 
-  @genre 
-end 
 
-def turn_page
-  puts "Flipping the page...wow, you read fast!"
-end 
 
-end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Book
+#   attr_accessor :author, :page_count, :genre 
+#   attr_reader :tittle 
+  
+#   def initialize(title)
+#     @title = title 
+#   end 
+  
+#   def title 
+#     @title
+#   end 
+  
+#   def author=(author)
+#     @author = author 
+# end 
+
+# def author 
+#   @author 
+# end 
+
+# def page_count(num)
+#   @page_count = num 
+# end 
+
+# def page_count
+#   @page_count 
+# end 
+
+# def genre=(genre)
+#   @genre = genre 
+# end 
+
+# def genre 
+#   @genre 
+# end 
+
+# def turn_page
+#   puts "Flipping the page...wow, you read fast!"
+# end 
+
+# end 
 
